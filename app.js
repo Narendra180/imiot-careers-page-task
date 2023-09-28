aa700321a9d8baa42024951fcbc048e62e42b243
narendra180 <narendrakanigiri18@gmail.com>
2023-09-29 02:08:57 +0530

    finished hero section styling

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const mobileNavToggleBtn = document.querySelector(".mobile-nav-links-toggle-btn");
-const navLinkUl = document.querySelector(".nav-links-ul");
+const headerNavLinksUl = document.querySelector(".nav-links-ul");
 
 mobileNavToggleBtn.addEventListener('click', () => {
-  navLinkUl.classList.add("visible")
+  const isVisible = headerNavLinksUl.classList.contains("visible");
+  if(isVisible) {
+    headerNavLinksUl.classList.remove("visible")
+  } else {
+    headerNavLinksUl.classList.add("visible");
+    headerNavLinksUl.focus();
+  }
+})
+
+headerNavLinksUl.addEventListener('blur', (e) => {
+  const relatedTarget = e.relatedTarget;
+  if(!headerNavLinksUl.contains(relatedTarget) && (relatedTarget !== mobileNavToggleBtn)) {
+    headerNavLinksUl.classList.remove("visible");
+  }
+})
+
+headerNavLinksUl.addEventListener('click', (e) => {
+  // e.preventDefault();
+  if(e.currentTarget)
+    e.currentTarget.classList.remove("visible");
 })
